e4b4ede5dd091ea0b5c374aad6fa1282d21cf922
arpho <damicogiuseppe77@gmail.com>
2022-06-21 10:28:57 +0200

    credentials
     set the credentials for the ptoject

---
+++ src/app/configs/credentials.ts
@@ -0,0 +1,59 @@
+export const configs = {
+  firebase: {
+    apiKey: "AIzaSyAnaDF6ofliYjJ9K5ONwEn6Cg2VRHsy84s",
+  authDomain: "scoutf24-67f89.firebaseapp.com",
+  databaseURL: "https://scoutf24-67f89-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "scoutf24-67f89",
+  storageBucket: "scoutf24-67f89.appspot.com",
+  messagingSenderId: "510250601417",
+  appId: "1:510250601417:web:638b1522edd99589917618",
+  measurementId: "G-ZD13DGMJDE"
+  },
+  google: {
+    api_key: 'AIzaSyDznBFTvvbLzPqgmAd7hC_fUbKGPzL_F6E'// 'AIzaSyClc8roQ21e1VFTzJTS0XkG7pVA_c3SGis'
+    
+  }
+};
+
+/* { fdirewbase's rules
+  "rules": {
+  
+        "categorie":{
+          "$uid":{
+            ".read":"$uid===auth.uid",
+              ".write":"$uid===auth.uid"
+          }
+        },
+          "fornitori":{
+          "$uid":{
+							".read":"$uid===auth.uid",
+              ".write":"$uid===auth.uid"
+          }
+        },
+          "acquisti":{
+          "$uid":{
+							".read":"$uid===auth.uid",
+              ".write":"$uid===auth.uid"
+          }
+        },
+          "userProfile":{
+          "$uid":{
+							".read":"$uid===auth.uid",
+              ".write":"$uid===auth.uid"
+          }
+        },
+          "widgets":{
+          "$uid":{
+							".read":"$uid===auth.uid",
+              ".write":"$uid===auth.uid"
+          }
+        },
+          "pagamenti":{
+          "$uid":{
+							".read":"$uid===auth.uid",
+              ".write":"$uid===auth.uid"
+          }
+        }
+
+}
+} */
