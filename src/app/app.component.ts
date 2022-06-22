01421767adaa1cac982a6dbefe3960de1a65d99a
arpho <damicogiuseppe77@gmail.com>
2022-06-22 19:32:20 +0200

    logged user
    impèlemented code to handle the logged user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import {configs} from "./configs/credentials"
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { configs } from "./configs/credentials"
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -17,10 +18,23 @@ export class AppComponent implements OnInit {
     { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() {}
+  constructor(public router: Router) { }
   ngOnInit(): void {
-  
     const app = initializeApp(configs.firebase)
     const auth = getAuth()
+    onAuthStateChanged(auth,async (user)=>{
+      if(user){
+        const token = await user.getIdTokenResult(true)
+        console.log("user ok è",user)
+        console.log("token.claims",token.claims)}
+        else{
+          this.router.navigate(["/users/login"])
+        }
+    }
+    
+    )
+
   }
+
 }
+  
