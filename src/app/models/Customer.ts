6b9c062e0a5dfd05b207405b7b0ee82d912b6309
arpho <damicogiuseppe77@gmail.com>
2022-06-24 19:44:39 +0200

    cusdtomer
    copied customer model from trasporto studenti

---
+++ src/app/models/Customer.ts
@@ -0,0 +1,117 @@
+import { Address } from "./Address"
+import { DateModel } from "../modules/user/models/birthDateModel"
+import { UserModel } from "../modules/user/models/userModel"
+import { UsersType } from "./usersType"
+import { Telephone } from "./telephone"
+import { Value } from "../modules/item/models/value"
+import { configs } from "../configs/configs"
+import { UserTypeModel } from "../modules/user/models/UserTypeModel"
+import { RoleModel } from "../modules/user/models/privilegesLevelModel"
+import { Serializers } from "../modules/helpers/serializers"
+
+export class Customer extends UserModel {
+    address: Address
+    userType: UsersType
+    telephones: Array<Telephone> = []
+    dor = new DateModel(new Date()) // date of registration
+    role = new RoleModel({ key: "Utente standard", value: 3 })
+
+    load(v: {}) {
+        this.telephones = []
+        Object.assign(this, v)
+        this.role = super.roleFactory(this.level)
+        if(v&&v['dor']){
+        this.dor = new DateModel(new Date(this.dor))}
+        if (v && v['telephones']) {
+            this.telephones = v['telephones'].map((t) => {
+                return new Telephone(t)
+            })
+        }
+        if (v && v['indirizzo']) {
+            this.address = new Address(v['indirizzo'])
+        }
+
+		else{
+			this.address = new Address()
+		}
+        if(!this.dor ){ // I assume user from db has a valid dor, if not is a a faulty user
+            this.dor = new DateModel(new Date("01-01-1972")) // set a very old Date this user has never been registered 
+        }
+
+        return this
+    }
+
+    serialize() {
+      const  serializers = new Serializers()
+
+        const telephones = this.telephones.map((t: Telephone) => t.serialize())
+
+        var out = {
+            ...super.serialize(),
+            ...{
+				level:serializers.serialize2PositiveNumber( this.level,3),
+                telephones: telephones,
+                archived: !!this.archived,
+                dor: new DateModel(this.dor).formatDate(),
+                userType: this.userType
+            }
+        }
+        if (this.key) {
+            out = { ...out, ...{ key: this.key } }
+        }
+        if (this.address instanceof Address) {
+            out = { ...out, ...{address: this.address.serialize() }, }
+        }
+
+        return out
+
+    }
+
+    getValue2(): Value {
+      return new Value({
+        value:this.email,
+        label:"email"
+      })
+    }
+
+
+    getValue4(): Value {
+        return new Value({ label: 'key', value: this.key })
+    }
+
+    getValue3(): Value {
+        return new Value({ value: this.getUserTypeKey(this.userType), label: 'utente' })
+    }
+
+    getUserTypeKey(level) {
+
+        var out = configs.userType.filter(
+            (access: UserTypeModel) => access.value === level
+        )[0]
+
+        return out ? out.key : ''
+
+    }
+
+    setKey(key: string) {
+        this.key = key
+        return this
+    }
+
+    getTitle(): Value {
+        return new Value({ value: `${this.firstName} ${this.lastName}`, label: 'utente' })
+    }
+
+    getElement(): { element: string; genere: "o" } {
+        return { element: 'utente', genere: 'o' }
+    }
+
+    constructor(user?: {}, key?: string) {
+        super(user, key)
+        this.load(user)
+        if (!this.userType) {
+            this.userType = UsersType.athlete
+        }
+
+    }
+}
