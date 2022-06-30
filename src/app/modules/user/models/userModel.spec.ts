068d2e0dee19ec4323e3de5bfd2dbce7319f49d3
arpho <damicogiuseppe77@gmail.com>
2022-06-30 11:23:42 +0200

    corrected  typo on tests

--- src/app/modules/user/models/userModel.spec.ts
+++ src/app/modules/user/models/userModel.spec.ts
@@ -5,14 +5,6 @@ import { RoleModel } from "./privilegesLevelModel";
 import { UserModel } from "./userModel";
 import { DateModel } from "./birthDateModel";
 const user = new UserModel();
-const val = {
-  email: "a@b.c",
-  firstName: "joe",
-  lastName: "friend",
-  level: 1,
-  birthDate: { day: 16, month: 2, year: 1977 },
-  enabled: true
-};
 describe("test build function", () => {
   const Val = {
     email: "a@b.c",
@@ -23,14 +15,22 @@ describe("test build function", () => {
     enabled: true
   };
   it("the user is build correctly", () => {
-    user.build(Val);
+const val = {
+  email: "a@b.c",
+  firstName: "joe",
+  lastName: "friend",
+  level: 0,
+  birthDate: { day: 16, month: 2, year: 1977 },
+  enabled: true
+};
+    user.build(val);
     expect(user.firstName).toBe(val.firstName);
     expect(user.lastName).toBe(val.lastName);
     expect(user.email).toBe(val.email);
     expect(user.birthDate.day).toBe(16);
     expect(user.birthDate.month).toBe(2);
     expect(user.birthDate.year).toBe(1977);
-    expect(user.level).toBe(1);
+    expect(user.level).toBe(0);
     expect(user.birthDate instanceof DateModel).toBeTruthy();
     // checkingf privileges level
     expect(user.role instanceof RoleModel).toBeTruthy();
@@ -39,10 +39,17 @@ describe("test build function", () => {
     expect(user.roleFactory(1).key).toBe("Responsabile")
     expect(user.roleFactory(2).key).toBe("Utente standard")
     expect(user.role.key).toBe("Sviluppatore")
-    expect(user.role.value).toBe(1);
+    expect(user.role.value).toBe(0);
   });
 });
-describe("serialize should work", () => {
+describe("serialize should work", () => {const val = {
+  email: "a@b.c",
+  firstName: "joe",
+  lastName: "friend",
+  level: 1,
+  birthDate: { day: 16, month: 2, year: 1977 },
+  enabled: true
+};
   user.build(val);
   it("the user is serialized", () => {
     const serializedUser = user.serialize();
